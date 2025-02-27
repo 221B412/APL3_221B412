695f34e310e7a8914ea3b7e19905df6b084fb274
221B412 <221B412@juet.co.in>
2025-02-27 11:29:32 +0530

    LE-4

---
+++ LE4/LazySolution/Singleton.java
@@ -0,0 +1,9 @@
+public class Singleton{
+    private static Singleton uniqueInstance;
+    private Singleton(){};
+    private static Singleton getInstance(){
+        if(uniqueInstance == null){
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }}
